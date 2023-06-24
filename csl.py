6a858d3ad065ac9f5039892ac8d524ddd674b0d9
Vinicius Melchior <viniciusmelchior.vmls@gmail.com>
2023-06-24 14:43:24 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
    	new file:   .idea/.gitignore
    	new file:   .idea/TP2.iml
    	new file:   .idea/inspectionProfiles/Project_Default.xml
    	new file:   .idea/inspectionProfiles/profiles_settings.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   __pycache__/csl.cpython-310.pyc
    	new file:   csl.py
    	new file:   main.py

---
+++ csl.py
@@ -0,0 +1,272 @@
+from datetime import date
+import time
+
+
+class AVLNode:
+    def __init__(self, nome, data, horario):
+        self.nome = nome
+        self.data = data
+        self.horario = horario
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, nome, data, horario):
+        self.root = self._insert_node(self.root, nome, data, horario)
+
+    def _insert_node(self, root, nome, data, horario):
+        if not root:
+            return AVLNode(nome, data, horario)
+        elif nome < root.nome:
+            root.left = self._insert_node(root.left, nome, data, horario)
+        else:
+            root.right = self._insert_node(root.right, nome, data, horario)
+
+        root.height = 1 + max(self._get_height(root.left), self._get_height(root.right))
+
+        balance_factor = self._get_balance_factor(root)
+
+        if balance_factor > 1 and nome < root.left.nome:
+            return self._rotate_right(root)
+
+        if balance_factor < -1 and nome > root.right.nome:
+            return self._rotate_left(root)
+
+        if balance_factor > 1 and nome > root.left.nome:
+            root.left = self._rotate_left(root.left)
+            return self._rotate_right(root)
+
+        if balance_factor < -1 and nome < root.right.nome:
+            root.right = self._rotate_right(root.right)
+            return self._rotate_left(root)
+
+        return root
+
+    def delete(self, nome):
+        self.root = self._delete_node(self.root, nome)
+
+    def _delete_node(self, root, nome):
+        if not root:
+            return root
+        elif nome < root.nome:
+            root.left = self._delete_node(root.left, nome)
+        elif nome > root.nome:
+            root.right = self._delete_node(root.right, nome)
+        else:
+            if not root.left:
+                temp = root.right
+                root = None
+                return temp
+            elif not root.right:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self._get_min_value_node(root.right)
+            root.nome = temp.nome
+            root.data = temp.data
+            root.horario = temp.horario
+            root.right = self._delete_node(root.right, temp.nome)
+
+        root.height = 1 + max(self._get_height(root.left), self._get_height(root.right))
+
+        balance_factor = self._get_balance_factor(root)
+
+        if balance_factor > 1 and self._get_balance_factor(root.left) >= 0:
+            return self._rotate_right(root)
+
+        if balance_factor < -1 and self._get_balance_factor(root.right) <= 0:
+            return self._rotate_left(root)
+
+        if balance_factor > 1 and self._get_balance_factor(root.left) < 0:
+            root.left = self._rotate_left(root.left)
+            return self._rotate_right(root)
+
+        if balance_factor < -1 and self._get_balance_factor(root.right) > 0:
+            root.right = self._rotate_right(root.right)
+            return self._rotate_left(root)
+
+        return root
+
+    def _get_min_value_node(self, root):
+        if root is None or root.left is None:
+            return root
+
+        return self._get_min_value_node(root.left)
+
+    def _get_height(self, root):
+        if not root:
+            return 0
+        return root.height
+
+    def _get_balance_factor(self, root):
+        if not root:
+            return 0
+        return self._get_height(root.left) - self._get_height(root.right)
+
+    def _rotate_left(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+    def _rotate_right(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+
+agenda = AVLTree()
+
+
+def create_event():
+    nome = input('Digite o nome do evento: ')
+    data = input('Digite a data do evento: ')
+    horario = input('Digite o horário do evento: ')
+    agenda.insert(nome, data, horario)
+    print(f'Evento {nome} criado com sucesso.')
+
+
+def read_event():
+    nome = input('Digite o nome do evento que deseja visualizar: ')
+    event = agenda.root
+    while event:
+        if event.nome == nome:
+            print('x' * 24)
+            print(f'|    Nome: {event.nome}')
+            print(f'|    Data: {event.data}')
+            print(f'|    Horário: {event.horario}')
+            print('x' * 24)
+            return
+        elif nome < event.nome:
+            event = event.left
+        else:
+            event = event.right
+
+    print(f'O evento {nome} não foi encontrado.')
+
+
+def update_event():
+    nome = input('Digite o nome do evento que deseja atualizar: ')
+    event = agenda.root
+    while event:
+        if event.nome == nome:
+            data = input('Digite a nova data do evento: ')
+            horario = input('Digite o novo horário do evento: ')
+            event.data = data
+            event.horario = horario
+            print(f'Evento {nome} atualizado com sucesso.')
+            return
+        elif nome < event.nome:
+            event = event.left
+        else:
+            event = event.right
+
+    print(f'O evento {nome} não foi encontrado.')
+
+
+def delete_event():
+    nome = input('Digite o nome do evento que deseja deletar: ')
+    agenda.delete(nome)
+    print(f'Evento {nome} deletado com sucesso.')
+
+
+def filter_events_today():
+    current_date = date.today().strftime("%d-%m-%Y")
+    filter_events_today_helper(agenda.root, current_date)
+
+
+def filter_events_today():
+    current_date = date.today().strftime('%d/%m/%Y')
+    filter_events_today_helper(agenda.root, current_date)
+
+
+def filter_events_today():
+    current_date = date.today().strftime('%d/%m/%Y')
+    events = []
+    filter_events_today_helper(agenda.root, current_date, events)
+    events.sort(key=lambda event: event.horario)
+
+    for event in events:
+        print('x' * 24)
+        print(f'|    Nome: {event.nome}')
+        print(f'|    Data: {event.data}')
+        print(f'|    Horário: {event.horario}')
+        print('x' * 24)
+
+
+def filter_events_today_helper(node, current_date, events):
+    if node is None:
+        return
+
+    filter_events_today_helper(node.left, current_date, events)
+
+    if node.data == current_date:
+        events.append(node)
+
+    filter_events_today_helper(node.right, current_date, events)
+
+
+def show_all_events():
+    show_all_events_helper(agenda.root)
+
+
+def show_all_events_helper(node):
+    if node is None:
+        return
+
+    show_all_events_helper(node.left)
+    print('x' * 24)
+    print(f'|    Nome: {node.nome}')
+    print(f'|    Data: {node.data}')
+    print(f'|    Horário: {node.horario}')
+    print('x' * 24)
+    show_all_events_helper(node.right)
+
+
+def optionmains():
+    print('[1 - Criar Evento        ]'
+          '\n[2 - Buscar Evento       ]'
+          '\n[3 - Atualizar Evento    ]'
+          '\n[4 - Deletar Evento      ]'
+          '\n[5 - Eventos para Hoje   ]'
+          '\n[6 - Todos os Eventos    ]')
+
+
+def options_menu(option: int):
+    if option == 1:
+        create_event()
+        time.sleep(2)
+    elif option == 2:
+        read_event()
+        time.sleep(2)
+    elif option == 3:
+        update_event()
+        time.sleep(2)
+    elif option == 4:
+        delete_event()
+        time.sleep(2)
+    elif option == 5:
+        filter_events_today()
+        time.sleep(2)
+    elif option == 6:
+        show_all_events()
+        time.sleep(2)
+    else:
+        print('Opção invalida')
---
+++ main.py
@@ -0,0 +1,14 @@
+import csl as modelers
+
+while True:
+    print('<>'*5+' MENU '+'<>'*5)
+    modelers.optionmains()
+    print('<>' * 13)
+    opmain = int(input('Opção: '))
+    modelers.options_menu(opmain)
+
+
+
+
+
+
